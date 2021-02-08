ad37743c4419cb65a38ef1018f68307957755495
Exploreidea <67377771+Exploreidea@users.noreply.github.com>
2021-02-08 23:37:25 +0600

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,29 @@
+(function($) {
+    'use strict';
+
+    $('.html').rProgressbar({
+        percentage: 99,
+        fillBackgroundColor: '#E34E29'
+    });
+    $('.css').rProgressbar({
+        percentage: 97,
+        fillBackgroundColor: '#304AD7'
+    });
+
+    $('.Wordpress').rProgressbar({
+        percentage: 88,
+        fillBackgroundColor: '#009AC8'
+    });
+
+    $('.javascript').rProgressbar({
+        percentage: 86,
+        fillBackgroundColor: '#EFD842'
+    });
+
+    $('.Shopify').rProgressbar({
+        percentage: 94,
+        fillBackgroundColor: '#8FBF55'
+    });
+
+
+})(jQuery);
